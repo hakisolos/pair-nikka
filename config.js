29b6ce5269241e0986b4252709dd7bac233129b1
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-02-16 11:07:51 +0100

    GOD DID

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  PREFIX: process.env.PREFIX || "NIKKA-X",
+  EMAIL: process.env.EMAIL || "maxwellexcel2@gmail.com",
+  PASS: process.env.PASS || "mynameisexcel2",
+  MESSAGE: process.env.MESSAGE || "Thank you for choosing nikka tech",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
 }
