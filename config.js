a5b046783364d47754368586849664de3c482b86
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-03-02 11:24:52 -0800

    GOD DID

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PREFIX: process.env.PREFIX || "NIKKA-X",
-  EMAIL: process.env.EMAIL || "maxwellexcel2@gmail.com",
+  EMAIL: process.env.EMAIL || "maxwellexcel174@gmail.com",
   PASS: process.env.PASS || "mynameisexcel2@",
   MESSAGE: process.env.MESSAGE || "Thank you for choosing nikka tech",
   IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/hvlui0.jpg",
